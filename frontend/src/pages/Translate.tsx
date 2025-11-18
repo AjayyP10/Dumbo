0757528532cfcb5bbe921f7c2da319aca051c9b4
Ajayy <ajayypannicker@gmail.com>
2025-11-18 16:28:56 +0100

    Improve empty state logo to Heroicons globe-alt outline in Translate page and OutputCard

--- frontend/src/pages/Translate.tsx
+++ frontend/src/pages/Translate.tsx
@@ -341,7 +341,7 @@ export default function Translate() {
                         <path
                           strokeLinecap="round"
                           strokeLinejoin="round"
-                          d="M12 6v6h4m-6 4h6m7 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                          d="M21 12a9 9 0 0 1-9 9m9-9a9 9 0 0 0-9-9m9 9H3m9 9a9 9 0 0 1-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 0 1 9-9"
                         />
                       </svg>
                       <p>Your translated text will appear here.</p>
